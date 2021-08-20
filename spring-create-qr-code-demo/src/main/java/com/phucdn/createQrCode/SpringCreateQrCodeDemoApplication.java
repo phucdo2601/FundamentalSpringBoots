07edaf036a797b01c6cfdb39bf81ae6e3aa2379d
phucdo2601 <ngocphucdo2601@gmail.com>
2021-08-20 23:12:40 +0700

    Implement create basic QR-CODE use Spring-Boot

---
+++ spring-create-qr-code-demo/src/main/java/com/phucdn/createQrCode/SpringCreateQrCodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.phucdn.createQrCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCreateQrCodeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCreateQrCodeDemoApplication.class, args);
+	}
+
+}
---
+++ spring-create-qr-code-demo/src/test/java/com/phucdn/createQrCode/SpringCreateQrCodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucdn.createQrCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCreateQrCodeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
