9393a6781e1e2f913b80f0bc11a2526f4291271c
phucdo2601 <ngocphucdo2601@gmail.com>
2021-09-04 19:42:21 +0700

    Learn Basic Rest AIP for Spring boot

---
+++ spring-learn-rest-api-basic-1/src/main/java/com/phucdn/learnRestAPI/SpringLearnRestApiBasic1Application.java
@@ -0,0 +1,13 @@
+package com.phucdn.learnRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnRestApiBasic1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnRestApiBasic1Application.class, args);
+	}
+
+}
---
+++ spring-learn-rest-api-basic-1/src/test/java/com/phucdn/learnRestAPI/SpringLearnRestApiBasic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucdn.learnRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnRestApiBasic1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
