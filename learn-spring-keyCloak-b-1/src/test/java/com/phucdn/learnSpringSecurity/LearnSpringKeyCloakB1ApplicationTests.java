d262913f55073ac88ae549a5bbf295d418b2ef39
phucdo2601 <ngocphucdo2601@gmail.com>
2021-11-12 22:08:10 +0700

    Learn Spring Spring Implement KeyCloak Basic 1

---
+++ learn-spring-keyCloak-b-1/src/test/java/com/phucdn/learnSpringSecurity/LearnSpringKeyCloakB1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucdn.learnSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringKeyCloakB1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
