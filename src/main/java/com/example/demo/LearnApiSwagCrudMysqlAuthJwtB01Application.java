25cb10fd276b61619fa68aa94229bd21eec40d24
phucdo2601 <ngocphucdo2601@gmail.com>
2023-01-04 22:48:04 +0700

    First learn Add and config authentication with jwt bearer token on swagger-ui b01

---
+++ src/main/java/com/example/demo/LearnApiSwagCrudMysqlAuthJwtB01Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class LearnApiSwagCrudMysqlAuthJwtB01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnApiSwagCrudMysqlAuthJwtB01Application.class, args);
+	}
+
+}
