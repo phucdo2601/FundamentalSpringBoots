c8a81038c8b042c350961d4fbc07de883665c009
Phuc Do <ngocphucdo2601@gmail.com>
2023-01-05 11:51:29 +0700

    First commit code of project b01

---
+++ src/main/java/com/example/demo/Les06DeployApiOnAzureB01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les06DeployApiOnAzureB01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les06DeployApiOnAzureB01Application.class, args);
+	}
+
+}
