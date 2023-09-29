178510539e78a7ca8bab6bca804f5a67fbe31275
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-09-29 11:10:59 +0700

    [java-learn-bas-micro-serv-b02] 03.Basic impl calling data by Resttemplate or WebClient on another system b01

--- microservice/fun/learn-ws-mc-art-b02/l-employee-service/src/main/java/com/phucdn/employeeapp/config/EmpoyeeAppConfig.java
+++ microservice/fun/learn-ws-mc-art-b02/l-employee-service/src/main/java/com/phucdn/employeeapp/config/EmpoyeeAppConfig.java
@@ -1,12 +1,22 @@
 package com.phucdn.employeeapp.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class EmpoyeeAppConfig {
+	/**
+	 * Class config beans for using in the app
+	 * In this app, using ModelMapper, RestTemplate, WebClient
+	 * @return
+	 */
+	
+	@Value("${addresservice.base.url}")
+	private String addressBaseUrl;
 	
 	@Bean
 	public ModelMapper modelMapper() {
@@ -14,8 +24,16 @@ public class EmpoyeeAppConfig {
 		return new ModelMapper();
 	}
 	
+//	@Bean
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
+	
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public WebClient webClient() {
+		return WebClient
+				.builder()
+				.baseUrl(addressBaseUrl)
+				.build();
 	}
 }
--- microservice/fun/learn-ws-mc-art-b02/l-employee-service/src/main/java/com/phucdn/employeeapp/service/employee/EmployeeServiceImpl.java
+++ microservice/fun/learn-ws-mc-art-b02/l-employee-service/src/main/java/com/phucdn/employeeapp/service/employee/EmployeeServiceImpl.java
@@ -1,20 +1,29 @@
 package com.phucdn.employeeapp.service.employee;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.phucdn.employeeapp.dto.response.AddressResponse;
 import com.phucdn.employeeapp.dto.response.EmployeeResponse;
 import com.phucdn.employeeapp.model.Employee;
 import com.phucdn.employeeapp.repository.EmployeeRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
@@ -25,8 +34,32 @@ public class EmployeeServiceImpl implements EmployeeService{
 	@Autowired
 	private ModelMapper modelMapper;
 	
-	@Autowired
+//	@Autowired
+//	private RestTemplate restTemplate;
+	
+	/**
+	 * Config restTempate on EmployeeService class manually, with set root uri
+	 */
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient webClient;
+	
+	/**
+	 * set base url on application.properties for calling calling api
+	 * @param addressBaseUrl
+	 * @param builder
+	 */
+//	@Value("${addresservice.base.url}")
+//	private String addressBaseUrl;
+	
+	public EmployeeServiceImpl(@Value("${addresservice.base.url}") String addressBaseUrl, RestTemplateBuilder builder) {
+		// TODO Auto-generated constructor stub
+		System.out.println(addressBaseUrl);
+		this.restTemplate = builder
+				.rootUri(addressBaseUrl)
+				.build();
+	}
 	
 	@Override
 	public List<Employee> getAllEmployees() {
@@ -55,14 +88,43 @@ public class EmployeeServiceImpl implements EmployeeService{
 //			obj = (List<AddressResponse>) restTemplate.get("http://localhost:8081/address-app/api/address/{id}", AddressResponse.class, id);
 			
 		/**
-		 * In this situation, using this case for solcing problem
+		 * In this situation, using this case for solving problem,
+		 * set base url on application.properties for calling calling api
 		 */
-			ResponseEntity<List<AddressResponse>> rateResponse =
-			        restTemplate.exchange("http://localhost:8081/address-app/api/address/{id}",
-			                    HttpMethod.GET, null, new ParameterizedTypeReference<List<AddressResponse>>() {
-			            }, id);
-			List<AddressResponse> addressResponses = rateResponse.getBody();
+//			ResponseEntity<List<AddressResponse>> rateResponse =
+//			        restTemplate.exchange(addressBaseUrl+"/address/{id}",
+//			                    HttpMethod.GET, null, new ParameterizedTypeReference<List<AddressResponse>>() {
+//			            }, id);
+//			ResponseEntity<List<AddressResponse>> rateResponse =
+//			        restTemplate.exchange("/address/{id}",
+//			                    HttpMethod.GET, null, new ParameterizedTypeReference<List<AddressResponse>>() {
+//			            }, id);
+//			List<AddressResponse> addressResponses = rateResponse.getBody();
+//			employeeResponse.setAddressResponse(addressResponses);
 			
+			// using webClient for calling api
+			/**
+			 * For response object model
+			 */
+//			AddressResponse addressResponse = webClient
+//					.get()
+//					.uri("/address/"+id)
+//					.retrieve()
+//					.bodyToMono(AddressResponse.class).block();
+			
+			/**
+			 * For response list data
+			 * Using Flux for calling api and after that, using the calling result  .collect(Collectors.toList())
+					.share().block();
+			 */
+			Flux<AddressResponse> addressFlux = webClient
+					.get()
+					.uri("/address/"+id)
+					.retrieve()
+					.bodyToFlux(AddressResponse.class);
+			
+			List<AddressResponse> addressResponses = addressFlux.collect(Collectors.toList())
+					.share().block();
 			employeeResponse.setAddressResponse(addressResponses);
 			
 			return employeeResponse;
