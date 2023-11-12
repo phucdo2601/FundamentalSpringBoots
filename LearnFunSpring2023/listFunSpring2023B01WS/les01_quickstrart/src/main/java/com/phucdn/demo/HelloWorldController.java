8766f9e580612da5a1f167c32954d62b9c8ad805
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-12 22:09:24 +0700

    [learnc-fun-spring-boot-api-2023-b01] 02.Test pushed code b01

--- LearnFunSpring2023/listFunSpring2023B01WS/les01_quickstrart/src/main/java/com/phucdn/demo/HelloWorldController.java
+++ LearnFunSpring2023/listFunSpring2023B01WS/les01_quickstrart/src/main/java/com/phucdn/demo/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@GetMapping(path = "/hello")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
