d03e3872acfbe0a9ec7495d95aa95afcf33b67f6
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-12 22:03:43 +0700

    [learnc-fun-spring-boot-api-2023-b01] 01.Getting Startted

---
+++ LearnFunSpring2023/listFunSpring2023B01WS/les01_quickstrart/src/test/java/com/phucdn/demo/Les01QuickstrartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucdn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les01QuickstrartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
