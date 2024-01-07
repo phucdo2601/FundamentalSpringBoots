65fd2fa431d358afbf8906d2f93a22a4c855a72e
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-01-07 15:58:27 +0700

    [prac-spring-mvc-2024-b01] 01. Basic create and set up project spring mvc b01

---
+++ LearnFunSpring2023/listFunSpring2023B01WS/springmvc-les01-pracPro-2024-b01/src/main/java/com/phucdn/demo/SpringmvcLes01PracPro2024B01Application.java
@@ -0,0 +1,13 @@
+package com.phucdn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcLes01PracPro2024B01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcLes01PracPro2024B01Application.class, args);
+	}
+
+}
