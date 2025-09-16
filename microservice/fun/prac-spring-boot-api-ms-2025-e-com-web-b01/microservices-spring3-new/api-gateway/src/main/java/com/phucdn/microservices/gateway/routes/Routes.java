4bffb5734f37d00bd63554c1cdeb93aa4d16b2d1
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-09-16 10:56:29 +0700

    [prac-spring-mc-ecom-2025-b01] [microservice-spring3-new] 06. Basic set up spring api doc swagger for api-gateway b01

--- microservice/fun/prac-spring-boot-api-ms-2025-e-com-web-b01/microservices-spring3-new/api-gateway/src/main/java/com/phucdn/microservices/gateway/routes/Routes.java
+++ microservice/fun/prac-spring-boot-api-ms-2025-e-com-web-b01/microservices-spring3-new/api-gateway/src/main/java/com/phucdn/microservices/gateway/routes/Routes.java
@@ -1,5 +1,6 @@
 package com.phucdn.microservices.gateway.routes;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -14,20 +15,48 @@ public class Routes {
 	@Bean
 	public RouterFunction<ServerResponse> productServiceRoute() {
 		return GatewayRouterFunctions.route("product-service-direct")
-            .route(RequestPredicates.path("/api/product"),
-                   HandlerFunctions.http("http://localhost:8081")) // đúng port 8081
-            .build();
+				.route(RequestPredicates.path("/api/product"),
+						HandlerFunctions.http("http://localhost:8081")) // đúng port 8081
+				.build();
 	}
 
 	@Bean
 	public RouterFunction<ServerResponse> inventoryServiceRoute() {
 		return GatewayRouterFunctions.route("inventory-service-direct")
-			.route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8083")).build();
+				.route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8083"))
+				.build();
 	}
 
 	@Bean
 	public RouterFunction<ServerResponse> orderService() {
 		return GatewayRouterFunctions.route("order-service-direct")
-			.route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8082")).build();
+				.route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8082")).build();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+		return GatewayRouterFunctions.route("product_service_swagger")
+				.route(RequestPredicates.path("/aggregate/prac-spring3-mc-ecom-2025-b01-product-service/v3/api-docs"),
+						HandlerFunctions.http("http://localhost:8081"))
+				.filter(setPath("/api-docs"))
+				.build();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+		return GatewayRouterFunctions.route("order_service_swagger")
+				.route(RequestPredicates.path("/aggregate/prac-spring3-mc-ecom-2025-b01-order-service/v3/api-docs"),
+						HandlerFunctions.http("http://localhost:8082"))
+				.filter(setPath("/api-docs"))
+				.build();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+		return GatewayRouterFunctions.route("inventory_service_swagger")
+				.route(RequestPredicates.path("/aggregate/prac-spring3-mc-ecom-2025-b01-inventory-service/v3/api-docs"),
+						HandlerFunctions.http("http://localhost:8083"))
+				.filter(setPath("/api-docs"))
+				.build();
 	}
 }
--- microservice/fun/prac-spring-boot-api-ms-2025-e-com-web-b01/microservices-spring3-new/prac-spring3-mc-ecom-2025-b01-order-service/src/main/java/com/phucdn/microservices/order/client/InventoryClient.java
+++ microservice/fun/prac-spring-boot-api-ms-2025-e-com-web-b01/microservices-spring3-new/prac-spring3-mc-ecom-2025-b01-order-service/src/main/java/com/phucdn/microservices/order/client/InventoryClient.java
@@ -4,9 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory", url = "${inventory.url}") 
+// @FeignClient(name = "inventory", url = "${inventory.url}") 
 public interface InventoryClient {
-	@RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+	// @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+	// boolean isInStock(@RequestParam(name = "skuCode") String skuCode, @RequestParam(name = "quantity") Integer quantity);
+
+	@GetExchange("/api/inventory")
 	boolean isInStock(@RequestParam(name = "skuCode") String skuCode, @RequestParam(name = "quantity") Integer quantity);
 }
