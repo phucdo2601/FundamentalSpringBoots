46a32006ab74d6f1f6887953695d3bf971e4d7c3
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-09-26 15:39:09 +0700

    [learn-fun-java-fx] 03. Learn basic activities of scenebuilder and eclipse b01

--- javaFx/Fundamntal/listLearnFunWsB02/helloWorldB01/src/application/Main.java
+++ javaFx/Fundamntal/listLearnFunWsB02/helloWorldB01/src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -15,25 +17,32 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Button btn = new Button("Click me");
-			btn.setOnAction(new EventHandler<ActionEvent>() {
-				
-				@Override
-				public void handle(ActionEvent arg0) {
-					System.out.println("Hello World!");
-					
-				}
-			});
+//			Button btn = new Button("Click me");
+//			btn.setOnAction(new EventHandler<ActionEvent>() {
+//				
+//				@Override
+//				public void handle(ActionEvent arg0) {
+//					System.out.println("Hello World!");
+//					
+//				}
+//			});
+//			
+//			Button btnExit = new Button("Exit");
+//			btnExit.setOnAction(e -> System.exit(0));
+//			
+//			VBox root = new VBox();
+//			root.getChildren().addAll(btn, btnExit);
+//			Scene scene = new Scene(root, 500, 300);
+//			primaryStage.setTitle("My title");
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
 			
-			Button btnExit = new Button("Exit");
-			btnExit.setOnAction(e -> System.exit(0));
-			
-			VBox root = new VBox();
-			root.getChildren().addAll(btn, btnExit);
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root, 500, 300);
 			primaryStage.setTitle("My title");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- javaFx/Fundamntal/listLearnFunWsB02/helloWorldB01/src/module-info.java
+++ javaFx/Fundamntal/listLearnFunWsB02/helloWorldB01/src/module-info.java
@@ -1,5 +1,6 @@
-module helloWorld {
+module helloWorldB01 {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
