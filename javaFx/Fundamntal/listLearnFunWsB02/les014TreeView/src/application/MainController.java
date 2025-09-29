fa410b4abce0d957270fc000b71e677f541be184
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-09-29 10:15:17 +0700

    [learn-fun-java-fx] 13. Basic learn tree view

---
+++ javaFx/Fundamntal/listLearnFunWsB02/les014TreeView/src/application/MainController.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+public class MainController implements Initializable{
+	
+	@FXML
+	public TreeView<String> treeView;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		TreeItem<String> root = new TreeItem<String>("Root");
+		
+		TreeItem<String> nodeA = new TreeItem<>("nodeA");
+		TreeItem<String> nodeB = new TreeItem<>("nodeB");
+		TreeItem<String> nodeC = new TreeItem<>("nodeC");
+		TreeItem<String> nodeD = new TreeItem<>("nodeD");
+//		root.getChildren().add(nodeA);
+//		root.getChildren().add(nodeB);
+//		root.getChildren().add(nodeC);
+//		root.getChildren().add(nodeD);
+		root.getChildren().addAll(nodeA, nodeB, nodeC, nodeD);
+		
+		TreeItem<String> nodeA1 = new TreeItem<>("nodeA1");
+		TreeItem<String> nodeB1 = new TreeItem<>("nodeB1");
+		TreeItem<String> nodeC1 = new TreeItem<>("nodeC1");
+		TreeItem<String> nodeD1 = new TreeItem<>("nodeD1");
+		
+		nodeA.getChildren().addAll(nodeA1, nodeB1, nodeC1, nodeD1);
+		
+		treeView.setRoot(root);
+		
+	}
+
+}
---
+++ javaFx/Fundamntal/listLearnFunWsB02/les014TreeView/src/module-info.java
@@ -0,0 +1,6 @@
+module les014TreeView {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
