53153df1f06839eb3b0e5ab120668aca8fdeb600
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-09-30 09:50:17 +0700

    [learn-fun-java-fx] 29. JavaFX Charts : Add mutiple lines in Line Chart

--- javaFx/Fundamntal/listLearnFunWsB02/les028LineChart/src/application/MainController.java
+++ javaFx/Fundamntal/listLearnFunWsB02/les028LineChart/src/application/MainController.java
@@ -17,7 +17,24 @@ public class MainController {
 		series.getData().add(new XYChart.Data<String, Number>("Mar", 100));
 		series.getData().add(new XYChart.Data<String, Number>("Apr", 500));
 		series.getData().add(new XYChart.Data<String, Number>("May", 400));
-		series.setName("Month Pay");
-		lineChart.getData().add(series);
+		series.setName("Month Pay 1");
+		
+		XYChart.Series<String, Number> series2 = new XYChart.Series<String, Number>();
+		series2.getData().add(new XYChart.Data<String, Number>("Jan", 100));
+		series2.getData().add(new XYChart.Data<String, Number>("Feb", 200));
+		series2.getData().add(new XYChart.Data<String, Number>("Mar", 200));
+		series2.getData().add(new XYChart.Data<String, Number>("Apr", 400));
+		series2.getData().add(new XYChart.Data<String, Number>("May", 100));
+		series2.setName("Month Pay 2");
+		
+		XYChart.Series<String, Number> series3 = new XYChart.Series<String, Number>();
+		series3.getData().add(new XYChart.Data<String, Number>("Jan", 500));
+		series3.getData().add(new XYChart.Data<String, Number>("Feb", 100));
+		series3.getData().add(new XYChart.Data<String, Number>("Mar", 400));
+		series3.getData().add(new XYChart.Data<String, Number>("Apr", 300));
+		series3.getData().add(new XYChart.Data<String, Number>("May", 600));
+		series3.setName("Month Pay 3");
+		
+		lineChart.getData().addAll(series, series2, series3);
 	}
 }
