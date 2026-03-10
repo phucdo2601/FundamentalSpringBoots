e35602eb75018bb8f0d502579e5b41c9d172254e
phucdo2601 <ngocphucdo2601@gmail.com>
2026-03-10 22:25:38 +0700

    [l-fullstack-job-portal-platform-2026-b01 FE] 01. Basic init and install some lib b01

---
+++ Fundamental/listPro2026/JobFindPlat2026B01/FrontEnd/fe-react-job-portal-2026-b01/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MantineProvider } from "@mantine/core";
+
+function App() {
+  return (
+    <>
+      <MantineProvider>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </MantineProvider>
+    </>
+  );
+}
+
+export default App;
