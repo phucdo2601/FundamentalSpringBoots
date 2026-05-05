c673b69bf26d9ca6973d50ca20b32e8cdcdc3dbe
phucdo2601 <ngocphucdo2601@gmail.com>
2026-05-05 22:35:07 +0700

    [l-fullstack-job-portal-platform-2026-b01 BE] 02. Basic create project structure b01

--- Fundamental/listPro2026/JobFindPlat2026B01/BackEnd/job-web-prac-2026-ws-b01/LearnJobPortal2026B01/src/main/java/com/phucdn/jobportal/entity/User.java
+++ Fundamental/listPro2026/JobFindPlat2026B01/BackEnd/job-web-prac-2026-ws-b01/LearnJobPortal2026B01/src/main/java/com/phucdn/jobportal/entity/User.java
@@ -1,9 +1,20 @@
 package com.phucdn.jobportal.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.phucdn.jobportal.dto.AccountType;
+
+import lombok.Data;
+@Data
+@Document(collection = "users")
 public class User {
+	@Id
 	private String id;
 	private String name;
+	@Indexed(unique = true)
 	private String email;
 	private String password;
-//	private Account
+	private AccountType accountType;
 }
